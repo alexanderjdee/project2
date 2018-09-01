311610a231bd31f7d57babd74d67d0ba3d99d665
adee <adee4990@gmail.com>
2018-09-01 11:23:21 -0400

    Add location to blog model, added update routes to blog and uer models, and changed port to pull from local .env file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var exphbs = require("express-handlebars");
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || process.env.LOCAL_PORT;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,7 +26,7 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
