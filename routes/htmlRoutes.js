90af9c2933a2b534f8ebda9b0e5304917c1455d9
Yeiska <yeiska83@yahoo.com>
2018-09-02 16:50:20 -0400

    adding button search by on main page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,25 +3,16 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.welcome.findAll({}).then(function(dbwelcome) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbwelcome
-      });
-    });
+    res.render("mainPage");
+    // res.render("index", {
+    //   msg: "Welcome!",
+    //   examples: dbwelcome
+    // });
   });
 
   // Load example page and pass in an example by id
-  app.get("/welcome/:id", function(req, res) {
-    db.welcome
-      .findOne({
-        where: { id: req.params.id }
-      })
-      .then(function(dbwelcome) {
-        res.render("welcome", {
-          example: dbwelcome
-        });
-      });
+  app.get("/welcome", function(req, res) {
+    res.render("welcome");
   });
 
   // Render 404 page for any unmatched routes
