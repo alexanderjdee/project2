07fef852ca2e159e69f61432aecc2ea7c8711eee
Debora Galeli <deboragaleli@gmail.com>
2018-09-03 16:58:19 -0400

    Create blog page, add login and register functionality on the client side inside index.js, add authentication an regiter route inside htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,14 +6,38 @@ module.exports = function(app) {
     res.render("mainPage");
   });
 
-  // Load example page and pass in an example by id
+  // Login logic
+  app.post("/api/authentication", function(req, res) {
+    console.log("email", req.body.email);
+    console.log("password", req.body.password);
+
+    // Here is the logic to go to MySQL and do the database authentication 
+
+  });
+
+
+  // Login logic
+  app.post("/api/register", function(req, res) {
+    console.log("email", req.body.email);
+    console.log("password", req.body.password);
+
+    // Here is the logic to go to MySQL and do the database authentication 
+    
+  });
+
+  // Load Home page
   app.get("/welcome", function(req, res) {
     res.render("welcome");
   });
 
-  // Load registration page
+  // Load Registration page
   app.get("/register", function(req, res) {
     res.render("register");
+  });
+
+  // Load Blog page
+  app.get("/blog", function(req, res) {
+    res.render("blog");
   });
 
   // Render 404 page for any unmatched routes
